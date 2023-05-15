ddead81eb4d6d5bf90daa0b6acb52d2a97e8e474
zarcode <denciczarko@gmail.com>
2023-05-15 09:19:52 +0200

    refactor: division function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export const add = (a, b) => a + b;
 export const sub = (a, b) => a - b;
 export const multiply = (a, b) => a * b;
-export const divide = (a, b) => a / b;
+export const divide = (a, b) => (b != 0 ? a / b : null);
