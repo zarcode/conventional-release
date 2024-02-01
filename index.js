714e90d769e44d90ac48520fb05a66def3b3a7a4
zarcode <denciczarko@gmail.com>
2024-02-01 21:04:36 +0100

    chore: remove

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ export const add = (a, b) => a + b;
 export const sub = (a, b) => a - b;
 export const multiply = (a, b) => a * b;
 export const divide = (a, b) => (b != 0 ? a / b : null);
-export const nullable = undefined;
-export const nullable2 = null;
